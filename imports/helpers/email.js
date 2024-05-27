40df44555641a8662dad1dfe7f6f5726cfe80416
Ciarán <bubonicfred@gmail.com>
2024-05-27 10:12:54 +1000

    20240527 docs (#769)
    * docs: Document Participants.Preparer.js
    
    * docs: add documentation
    
    * chore: Deprecate old email validation functions and add new email validation function
    
    * feat: Add mongoUtils.js for MongoDB database connection and collection creation
    
    * style: format code with clang-format
    
    * style: format code with prettier
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- imports/helpers/email.js
+++ imports/helpers/email.js
@@ -2,6 +2,7 @@
  * Validates an email address using a regular expression.
  * @returns {boolean} `true` if the input string matches the email address
  *     regular expression, `false` otherwise.
+ * @deprecated
  */
 export const emailAddressRegExpTest = /^[^\s@]+@([^\s@]+){2,}\.([^\s@]+){2,}$/;
 /**
@@ -10,6 +11,20 @@ export const emailAddressRegExpTest = /^[^\s@]+@([^\s@]+){2,}\.([^\s@]+){2,}$/;
  * `[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z0-9._-]{2,}`
  * This includes common email address formats like `example@domain.com` and
  * `user.name@subdomain.example.com`.
+ * @deprecated
  */
 export const emailAddressRegExpMatch =
   /([a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z0-9._-]{2,})/gi;
+
+/**
+ * Regular expression pattern for validating email addresses.
+ * @type {RegExp}
+ */
+export const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+/**
+ * Checks if the given email is valid.
+ *
+ * @param {string} email - The email to be validated.
+ * @returns {boolean} - Returns true if the email is valid, otherwise false.
+ */
+export const isValidEmail = (email) => emailRegExp.test(email);
